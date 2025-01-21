ee11d49867b5e35ac58c687b0499d5fa334f52f4
Areesha <133148078+Areesha-Rizwan@users.noreply.github.com>
2025-01-21 09:30:09 -1200

    Update apiKeys.js

--- weatherapp/weatherApp-Reactjs/src/apiKeys.js
+++ weatherapp/weatherApp-Reactjs/src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "{afe283f762e6cb20215ee1bd96c43fb3}",
   base: "https://api.openweathermap.org/data/2.5/",
 };
